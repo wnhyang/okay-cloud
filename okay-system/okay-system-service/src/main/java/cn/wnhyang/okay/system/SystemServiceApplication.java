658fbd86c5ff9c9dc91f74ae9ba2198604802a41
wnhyang <wnhyang@qq.com>
2023-05-19 08:42:36 +0800

    system模块main

---
+++ okay-system/okay-system-service/src/main/java/cn/wnhyang/okay/system/SystemServiceApplication.java
@@ -0,0 +1,15 @@
+package cn.wnhyang.okay.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wnhyang
+ * @date 2023/5/19
+ **/
+@SpringBootApplication
+public class SystemServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemServiceApplication.class, args);
+    }
+}
