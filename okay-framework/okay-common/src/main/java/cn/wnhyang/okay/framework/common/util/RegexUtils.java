af608c39ae97d55a6108a246ed98a24bea047b2d
wnhyang <wnhyang@qq.com>
2023-10-07 16:18:55 +0800

    修改变量类型和注释格式

--- okay-framework/okay-common/src/main/java/cn/wnhyang/okay/framework/common/util/RegexUtils.java
+++ okay-framework/okay-common/src/main/java/cn/wnhyang/okay/framework/common/util/RegexUtils.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @UtilityClass
 @Slf4j
 public class RegexUtils {
-    public static String MOBILE = "^(?:(?:\\+|00)86)?1(?:(?:3[\\d])|(?:4[0,1,4-9])|(?:5[0-3,5-9])|(?:6[2,5-7])|(?:7[0-8])|(?:8[\\d])|(?:9[0-3,5-9]))\\d{8}$";
-    public static String EMAIL = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(?:\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$";
+    public static final String MOBILE = "^(?:(?:\\+|00)86)?1(?:(?:3[\\d])|(?:4[0,1,4-9])|(?:5[0-3,5-9])|(?:6[2,5-7])|(?:7[0-8])|(?:8[\\d])|(?:9[0-3,5-9]))\\d{8}$";
+    public static final String EMAIL = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(?:\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$";
 
 }
--- okay-framework/okay-spring-boot-starter-satoken/src/main/java/cn/wnhyang/okay/framework/satoken/utils/LoginHelper.java
+++ okay-framework/okay-spring-boot-starter-satoken/src/main/java/cn/wnhyang/okay/framework/satoken/utils/LoginHelper.java
@@ -35,12 +35,12 @@ public class LoginHelper {
         }
         // 自定义分配 不同用户体系 不同 token 授权时间 不设置默认走全局 yml 配置
         // 例如: 后台用户30分钟过期 app用户1天过期
-//        UserType userType = UserType.getUserType(loginUser.getUserType());
-//        if (userType == UserType.SYS_USER) {
-//            model.setTimeout(86400).setActiveTimeout(1800);
-//        } else if (userType == UserType.APP_USER) {
-//            model.setTimeout(86400).setActiveTimeout(1800);
-//        }
+        // UserType userType = UserType.getUserType(loginUser.getUserType());
+        // if (userType == UserType.SYS_USER) {
+        //  model.setTimeout(86400).setActiveTimeout(1800);
+        // } else if (userType == UserType.APP_USER) {
+        //  model.setTimeout(86400).setActiveTimeout(1800);
+        // }
         StpUtil.login(loginUser.getId(), model);
         StpUtil.getTokenSession().set(LOGIN_USER_KEY, loginUser);
     }
